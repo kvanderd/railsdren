1d478a850814b42baa9c4dc387d5010f123ffb36
kvanderd <me@katharine.org>
2013-08-08 11:45:54 -0700

    userlogin/bootstrap skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# Bootstrapuser::Application.config.secret_token = '3f2d4dadfb36eb84e1d18d3481f498df4b26ca7e6e2be87f0455dc273d7d6b6be81ba7448d1fc3a357c69a888f717365439002e67f35108d54295ef282ef8fdb'
