48e86d65552ab2800a1689944e25bbc010e3a6f2
kvanderd <me@katharine.org>
2013-08-10 10:42:04 -0700

    decks are now pointing at action ==> create, controller => rounds

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,15 +1,18 @@
 class RoundsController < ApplicationController
 
 	def new
-		@round = Round.create(deck_id: params[:deck_id])
-		session[@round_id]
-		@cards = Deck.find(params[:deck_id]).cards
-		#this puts the cards in the order they will be rendered
-		@shuffled_deck = @cards.map { |card| card.id }.shuffle.to_json
-		#this is a shuffled deck of card_ids.
-		#show one at a time. When 10 cards have been shown, end
-		@shuffled_deck
-		#should this redirect to show (pass the round session and shuffle the cards)
+		# @round = Round.create(deck_id: params[:deck_id])
+		# session[@round_id]
+		# @cards = Deck.find(params[:deck_id]).cards
+		# #this puts the cards in the order they will be rendered
+		# @shuffled_deck = @cards.map { |card| card.id }.shuffle.to_json
+		# #this is a shuffled deck of card_ids.
+		# #show one at a time. When 10 cards have been shown, end
+		# @shuffled_deck
+		# #should this redirect to show (pass the round session and shuffle the cards)
+	end
+
+	def create
 	end
   
   def show
